6876ca60294b67cc8a80791b44c2fa473b9f9302
Victor Savkin <mail@vsavkin.com>
2022-06-07 15:40:39 -0400

    init repo

---
+++ apps/crew/pages/index.tsx
@@ -0,0 +1,21 @@
+import styles from './index.module.css';
+import { Alert0 } from 'shared-alerts';
+// import { Button0 } from 'shared-buttons';
+// import { Component0 } from 'shared-components';
+// import { Dialog0 } from 'shared-dialogs';
+// import { Icon0 } from 'shared-icons';
+
+export function Index() {
+  return (
+    <div className={styles.page}>
+      <h2>Index Page</h2>
+      <Alert0/>
+      {/*<Button0/>*/}
+      {/*<Component0/>*/}
+      {/*<Dialog0/>*/}
+      {/*<Icon0/>*/}
+    </div>
+  );
+}
+
+export default Index;
---
+++ packages/warp-drive-manager/important-feature-1/jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  testMatch: ['**/+(*.)+(spec|test).+(ts|js)?(x)'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  transform: {
+    '^.+\.tsx?$': 'ts-jest'
+  },
+  testEnvironment: 'jsdom'
+};
